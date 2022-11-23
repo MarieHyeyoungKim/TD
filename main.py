1f52e1b401a9b891d9ccb16576c8bcb1b3c13c42
hyeyoung Kim <hyeyoungsummer@gmail.com>
2022-11-23 21:02:54 +0900

    add doxygen files

---
+++ main.py
@@ -0,0 +1,12 @@
+class ClassName(object):
+   """docstring for ClassName"""
+   def __init__(self, arg):
+      super(ClassName, self).__init__()
+      self.arg = arg
+
+   def function(self):
+    """안녕하세요?"""
+      pass
+      
+def function():
+   pass
